4adf8824532a2da678b44bca9e4b459a46aa9621
Jack Drogon <jack.xsuperman@gmail.com>
2018-04-12 15:48:36 +0800

    Add rispy.rb

---
+++ rispy/rispy-sample.scm
@@ -0,0 +1,12 @@
+(begin
+ (define fact (lambda (n) 
+  (if (<= n 1) 1 (* n (fact (- n 1))))))
+ 
+ (define f (lambda (return)
+   (begin
+    (return 2)
+    1)))
+ (display (f (lambda (x) x)))
+ (display (callcc f))
+ (fact 5)
+)
